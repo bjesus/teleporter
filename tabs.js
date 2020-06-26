cb5f80dfea0f6d84b0420e05d9ed03142d6b4633
Yo'av Moshe <bjesus@gmail.com>
2020-06-26 09:06:28 +0200

    * better search and sort * close tab by prepending 0 * show hidden tabs by prepending *

--- tabs.js
+++ tabs.js
@@ -1,9 +1,3 @@
-function fuzzyMatch(pattern, str) {
-  pattern = ".*" + pattern.split("").join(".*") + ".*";
-  const re = new RegExp(pattern);
-  return re.test(str);
-}
-
 const filterInput = document.getElementById("filterInput");
 
 function updateList() {
@@ -14,19 +8,43 @@ function updateList() {
  * listTabs to switch to
  */
 function listTabs(filter) {
-  getCurrentWindowTabs().then(tabs => {
+  let hidden = false;
+  if (typeof filter === "string") {
+    if (filter.startsWith("*")) {
+      hidden = true;
+      filter = filter.replace("*", "");
+    }
+  } else {
+    filter = "";
+  }
+  browser.tabs.query({ currentWindow: true }).then((tabs) => {
     let tabsList = document.getElementById("tabs-list");
     let currentTabs = document.createDocumentFragment();
-    let counter = 0;
-    console.log(filter);
+    let counter = Math.ceil(tabs.length / 10) + 2;
+
     tabsList.textContent = "";
     let matching = 0;
 
-    for (let tab of tabs) {
-      if (
-        typeof filter !== "string" ||
-        fuzzyMatch(filter.toLowerCase(), tab.title.toLowerCase())
-      ) {
+    let filteredTabs;
+    if (filter) {
+      fuzzySearch = fuzzysort.go(filter, tabs, { key: "title" });
+      filteredTabs = fuzzySearch.map((o) => o.obj.id);
+      tabs.sort(function (a, b) {
+        try {
+          return (
+            fuzzySearch.find((t) => t.obj.id == b.id).score -
+            fuzzySearch.find((t) => t.obj.id == a.id).score
+          );
+        } catch (e) {
+          return true;
+        }
+      });
+    } else {
+      filteredTabs = tabs.map((t) => t.id);
+    }
+
+    for (const tab of tabs) {
+      if (filteredTabs.includes(tab.id) && (hidden || !tab.hidden)) {
         matching = matching + 1;
         let tabHolder = document.createElement("div");
         tabHolder.classList.add("tab-holder");
@@ -46,7 +64,7 @@ function listTabs(filter) {
         tabHolder.appendChild(tabImg);
 
         let tabCode = document.createElement("span");
-        tabCode.textContent = counter + 11;
+        tabCode.textContent = counter;
         tabHolder.appendChild(tabCode);
 
         let tabLink = document.createElement("a");
@@ -58,7 +76,7 @@ function listTabs(filter) {
         let closeImg = document.createElement("img");
         closeImg.classList.add("closeButton");
         closeImg.setAttribute("src", "icons/close-icon.svg");
-        closeImg.onclick = function(event) {
+        closeImg.onclick = function (event) {
           const tabId = +event.target.previousSibling.getAttribute("href");
           browser.tabs.remove(tabId);
         };
@@ -77,15 +95,18 @@ function listTabs(filter) {
   });
 }
 
-document.onkeydown = function(event) {
+document.onkeydown = function (event) {
   if (event.keyCode == 38) {
     const currentTab = document.getElementsByClassName("active-tab")[0];
     const prevTabId = +currentTab.previousSibling
       .getElementsByTagName("a")[0]
       .getAttribute("href");
-    var updating = browser.tabs.update(prevTabId, {
-      active: true
+    browser.tabs.update(prevTabId, {
+      active: true,
     });
+    setTimeout(function () {
+      window.focus();
+    }, 2000);
   }
   if (event.keyCode == 40) {
     const currentTab = document.getElementsByClassName("active-tab")[0];
@@ -93,8 +114,11 @@ document.onkeydown = function(event) {
       .getElementsByTagName("a")[0]
       .getAttribute("href");
     browser.tabs.update(nextTabId, {
-      active: true
+      active: true,
     });
+    setTimeout(function () {
+      window.focus();
+    }, 2000);
   }
 };
 
@@ -102,19 +126,39 @@ browser.tabs.onCreated.addListener(updateList);
 browser.tabs.onRemoved.addListener(updateList);
 browser.tabs.onUpdated.addListener(updateList);
 
-filterInput.oninput = function() {
+filterInput.oninput = async function () {
   const numbers = this.value.replace(/\D/g, "");
-  console.log(numbers);
   if (numbers.length > 1) {
-    var xpath = "//span[text()='" + numbers + "']";
-    var matchingElement = document.evaluate(
-      xpath,
-      document,
-      null,
-      XPathResult.FIRST_ORDERED_NODE_TYPE,
-      null
-    ).singleNodeValue;
-    matchingElement.nextSibling.click();
+    if (numbers[0] === "0") {
+      var xpath = "//span[text()='" + numbers.substring(1) + "']";
+      // const tabId = +event.target.previousSibling.getAttribute("href");
+      var matchingElement = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      ).singleNodeValue;
+
+      const tabId = matchingElement.nextSibling.getAttribute("href");
+      if (tabId) {
+        browser.tabs.remove(+tabId).then((x) => {
+          filterInput.value = "";
+          listTabs("");
+          filterInput.focus();
+        });
+      }
+    } else {
+      var xpath = "//span[text()='" + numbers + "']";
+      var matchingElement = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      ).singleNodeValue;
+      matchingElement.nextSibling.click();
+    }
   } else {
     listTabs(this.value.replace(/[0-9]/g, ""));
   }
@@ -122,22 +166,18 @@ filterInput.oninput = function() {
 
 document.addEventListener("DOMContentLoaded", listTabs);
 
-function getCurrentWindowTabs() {
-  return browser.tabs.query({ currentWindow: true, hidden: false });
-}
-
-document.addEventListener("click", e => {
+document.addEventListener("click", (e) => {
   var tabId = +e.target.getAttribute("href");
 
   browser.tabs
     .query({
-      currentWindow: true
+      currentWindow: true,
     })
-    .then(tabs => {
+    .then((tabs) => {
       for (var tab of tabs) {
         if (tab.id === tabId) {
           browser.tabs.update(tabId, {
-            active: true
+            active: true,
           });
           window.close();
         }
